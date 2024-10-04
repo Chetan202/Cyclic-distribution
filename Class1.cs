ef2b0dc5544c5cb06871e9357a0d65d583effa25
chetanjha <chetanjha888@gmail.com>
2024-10-04 16:03:00 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cyclic_distribution
+{
+    public class Class1 : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Get the context, tracing service, and organization service from the service provider
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            tracingService.Trace("AssignCaseToUserUsingHashing Plugin execution started.");
+
+            // Check if the Target parameter exists and is of type Entity
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity caseEntity = (Entity)context.InputParameters["Target"];
+                tracingService.Trace("Target entity is: {0}", caseEntity.LogicalName);
+
+                // Ensure the entity is a case (incident)
+                if (caseEntity.LogicalName != "incident")
+                {
+                    tracingService.Trace("Entity is not a case. Exiting plugin.");
+                    return;
+                }
+
+                try
+                {
+                    tracingService.Trace("Retrieving all active users with the specified criteria.");
+
+                    // FetchXML to retrieve users based on provided criteria (e.g., positionid)
+                    string fetchXml = @"
+            <fetch version='1.0' output-format='xml-platform' mapping='logical' no-lock='false' distinct='true'>
+                <entity name='systemuser'>
+                    <attribute name='fullname'/>
+                    <attribute name='systemuserid'/>
+                    <filter type='and'>
+                        <condition attribute='isdisabled' operator='eq' value='0'/>
+                        <condition attribute='accessmode' operator='eq' value='0'/>
+                        <condition attribute='positionid' operator='eq' value='{4f0b53fa-cf74-ef11-ac20-6045bdc5d905}' uiname='Associate' uitype='position'/>
+                    </filter>
+                </entity>
+            </fetch>";
+
+                    // Retrieve the users based on the FetchXML
+                    EntityCollection users = service.RetrieveMultiple(new FetchExpression(fetchXml));
+                    tracingService.Trace("Total active users retrieved: {0}", users.Entities.Count);
+
+                    if (users.Entities.Count > 0)
+                    {
+                        // Get the case ID for hashing logic
+                        Guid caseId = caseEntity.Id;
+                        tracingService.Trace("Case ID: {0}", caseId);
+
+                        // Hash the case ID and distribute the cases cyclically among users
+                        int userIndex = GetUserIndexByHashing(caseId, users.Entities.Count);
+                        tracingService.Trace("User index calculated from hash: {0}", userIndex);
+
+                        Entity selectedUser = users.Entities[userIndex];
+                        tracingService.Trace("Assigning case to user: {0}", selectedUser["fullname"]);
+
+                        // Update the case with the selected user as the owner
+                        Entity updatedCaseEntity = new Entity(caseEntity.LogicalName, caseEntity.Id);
+                        updatedCaseEntity["ownerid"] = new EntityReference("systemuser", selectedUser.Id);
+                        service.Update(updatedCaseEntity);
+
+                        tracingService.Trace("Case assigned to user: {0}", selectedUser["fullname"]);
+                    }
+                    else
+                    {
+                        tracingService.Trace("No active users found based on the FetchXML criteria.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    tracingService.Trace("Error in AssignCaseToUserUsingHashing: {0}", ex.ToString());
+                    throw new InvalidPluginExecutionException("An error occurred in the AssignCaseToUserUsingHashing plugin.", ex);
+                }
+            }
+            else
+            {
+                tracingService.Trace("Target is not an entity.");
+            }
+
+            tracingService.Trace("AssignCaseToUserUsingHashing Plugin execution ended.");
+        }
+
+        // Helper method to calculate the index of the user to assign the case to using hashing logic
+        private int GetUserIndexByHashing(Guid caseId, int totalUsers)
+        {
+            // Get the hash code of the case ID
+            int hash = caseId.GetHashCode();
+
+            // Ensure the hash value is positive
+            if (hash < 0) hash = -hash;
+
+            // Calculate the user index by taking the modulus of the hash with the total number of users
+            int userIndex = hash % totalUsers;
+
+            return userIndex;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cyclic distribution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Cyclic distribution")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c4dbb3a-a90b-4d00-8466-2dd182a5da04")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
